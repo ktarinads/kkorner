cc9d22e32feb28c23050be781a877a32df066825
ktarinads <ktarinads@users.noreply.github.com>
2023-10-08 22:11:22 +0000

    deploy: e5051045c7c2510f0112ae4542026482d094e3ee

--- assets/js/5a63d8bd.ac37c25d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkdev_docs=self.webpackChunkdev_docs||[]).push([[156],{8129:e=>{e.exports=JSON.parse('{"title":"Structure overview","slug":"/category/structure","permalink":"/kkorner/category/structure","navigation":{"previous":{"title":"Thought drafts","permalink":"/kkorner/"},"next":{"title":"Structure","permalink":"/kkorner/category/structure-1"}}}')}}]);
